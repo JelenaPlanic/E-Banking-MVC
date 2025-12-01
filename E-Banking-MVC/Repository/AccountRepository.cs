eae1b415408f194be142e875537aac6701110bb9
jelenaLela <jelena.lela.planic@gmail.com>
2025-12-01 14:19:32 +0100

    add new methods (updateStatus, getTotal, searchBy) in Account Repo

--- E-Banking-MVC/Repository/AccountRepository.cs
+++ E-Banking-MVC/Repository/AccountRepository.cs
@@ -146,5 +146,112 @@ namespace E_Banking_MVC.Repository
             conn.Close();
         }
 
+        public void UpdateStatus(int AccountId, bool status)
+        {
+            string query = "update Account set IsActive = @status where AccountId = @id";
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = conn.CreateCommand();
+
+            conn.Open();
+            cmd.CommandText = query;
+            cmd.Parameters.AddWithValue("@id", AccountId);
+            cmd.Parameters.AddWithValue("@status", status);
+
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            conn.Close();
+        }
+
+        public decimal GetTotal(int AccountId)
+        {
+            string query = "select coalesce(sum(Amount),0) as total from Payment where AccountId = @id";
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = conn.CreateCommand();
+
+            conn.Open();
+            cmd.CommandText = query;
+            cmd.Parameters.AddWithValue("@id", AccountId);
+
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = cmd;
+            adapter.Fill(ds, "Payment");
+            dt = ds.Tables["Payment"];
+
+            cmd.Dispose();
+            conn.Close();
+
+            decimal total = 0;
+            foreach(DataRow row in dt.Rows)
+            {
+                total = decimal.Parse(row["total"].ToString());
+            }
+            return total;
+
+        }
+
+        public List<Account> SearchAccountsBy(string accountHolder, string accountNum, decimal? amountFrom, decimal? amountTo, bool? active)
+        {
+            string query = "select * from Account as a " +
+                           "left join (select Payment.AccountId, coalesce(sum(Amount),0) as total from Payment " +
+                           "group by Payment.AccountId) as p " +
+                           "on p.AccountId = a.AccountId " +
+                           "where AccountHolder like @holder " +
+                           "and AccountNumber like @number";
+
+            if(amountFrom != null)
+            {
+                query += " and total >= @amountFrom";
+            }
+            if(amountTo != null)
+            {
+                query += " and total <= @amountTo";
+            }
+            if(active != null)
+            {
+                query += " and IsActive = @active";
+            }
+
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = conn.CreateCommand();
+
+            conn.Open();
+            cmd.CommandText = query;
+            cmd.Parameters.AddWithValue("@holder", $"%{accountHolder ?? ""}%"); // if null AddWithValue: exception
+            cmd.Parameters.AddWithValue("@number", $"%{accountNum ?? ""}%"); // sql like%%
+            cmd.Parameters.AddWithValue("amountFrom", amountFrom ?? 0);
+            cmd.Parameters.AddWithValue("amountTo", amountTo ?? 0);
+            cmd.Parameters.AddWithValue("active", active ?? false);
+
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = cmd;
+            adapter.Fill(ds, "Account");
+            dt = ds.Tables["Account"];
+
+            cmd.Dispose();
+            conn.Close();
+
+            List<Account> accounts = new List<Account>();
+            foreach(DataRow row in dt.Rows)
+            {
+                Account account = new Account();
+                account.Id = int.Parse(row["AccountId"].ToString());
+                account.Holder = row["AccountHolder"].ToString();
+                account.Number = row["AccountNumber"].ToString();
+                account.IsActive = bool.Parse(row["IsActive"].ToString());
+                account.HasOnlineBanking = bool.Parse(row["HasOnlineBanking"].ToString());
+                accounts.Add(account);
+            }
+            return accounts;
+
+        }
+
     }
 }
--- E-Banking-MVC/Repository/Interfaces/IAccountRepository.cs
+++ E-Banking-MVC/Repository/Interfaces/IAccountRepository.cs
@@ -6,5 +6,16 @@ namespace E_Banking_MVC.Repository.Interfaces
     {
         List<Account> GetAll();
         Account GetOne(int AccountId);
+
+        void Create(Account account);
+        void Update(Account account);
+
+        void Delete(int AccountId);
+
+        void UpdateStatus(int AccountId, bool status);
+
+        decimal GetTotal(int AccountId);
+
+        List<Account> SearchAccountsBy(string accountHolder, string accountNum, decimal? ammountFrom, decimal? ammountTo, bool? active);
     }
 }
