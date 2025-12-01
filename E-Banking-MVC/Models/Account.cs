dd1f4a6cd1a98e69b4fb98a3238332b068b58bd4
jelenaLela <jelena.lela.planic@gmail.com>
2025-12-01 11:01:15 +0100

    Add Models

---
+++ E-Banking-MVC/Models/Account.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Banking_MVC.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Holder field is required")]
+        public string Holder { get; set; }
+
+        [Required(ErrorMessage = "The Account Number is required")]
+        [RegularExpression(@"^[a-zA-Z0-9_]+$", ErrorMessage = "You can enter only alphanumeric characters")]
+        public string Number { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool HasOnlineBanking { get; set; }
+    }
+}
---
+++ E-Banking-MVC/Models/Payment.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Banking_MVC.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+
+        [Required(ErrorMessage = "The Amount Field is required")]
+        public decimal Amount { get; set; }
+
+        [Required(ErrorMessage = "The TransactionDate Field is required")]
+        public DateTime TransactionDate { get; set; }
+
+        [Required(ErrorMessage = "The Purpose Field is required")]
+        public string Purpose { get; set; }
+
+        [Required(ErrorMessage = "The PayerName Field is required")]
+        public string PayerName { get; set; }
+        public bool IsUrgent { get; set; }
+        
+    }
+}
