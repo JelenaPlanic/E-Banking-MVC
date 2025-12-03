d307e0779463b01921498859c391ab3ba0399681
jelenaLela <jelena.lela.planic@gmail.com>
2025-12-03 10:56:34 +0100

    Add Index action and ViewModel for Index.cshtml

--- E-Banking-MVC/Controllers/PaymentController.cs
+++ E-Banking-MVC/Controllers/PaymentController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_Banking_MVC.Repository;
+using E_Banking_MVC.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace E_Banking_MVC.Controllers
 {
     public class PaymentController : Controller
     {
+        private IConfiguration Configuration { get; }
+        private IAccountRepository AccountRepository { get; }
+        private IPaymentRepository PaymentRepository { get; }
+
+        public PaymentController(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+            this.AccountRepository = new AccountRepository(Configuration);
+            this.PaymentRepository = new PaymentRepository(Configuration);
+        }
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
     }
 }
---
+++ E-Banking-MVC/ViewModels/PaymentPaymentsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using E_Banking_MVC.Models;
+
+namespace E_Banking_MVC.ViewModels
+{
+    public class PaymentPaymentsViewModel   // container for data
+    {
+        public List<Payment> Payments { get; set; }
+        public Account Account { get; set; }
+        public Payment Payment { get; set; }
+        public decimal Total { get; set; }
+
+    }
+}
